564a849dc45af30d3a00c9d7518b80b70cbfa53f
prosergeant <sberendeyev@gmail.com>
2026-01-01 17:44:56 +0500

    di/setup esbuild for new decorators stage 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueDevTools()],
+
+    // чтобы работали новые декораторы stage-3
+    esbuild: {
+        target: 'es2022',
     },
-  },
+
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
 })
